fb205d8abf72cd150253b1e4f894a3817e0a5b78
Higor P. <137324645+nupsbr@users.noreply.github.com>
2025-03-04 17:47:22 -0300

    add login email e senha

--- Main_Cadastro.py
+++ Main_Cadastro.py
@@ -26,17 +26,14 @@ def inserir_dados():
         conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
 
-        # input pelo terminal
         nome = input("\nNome: ")
         sobrenome = input("Sobrenome: ")
         idade = input("Idade: ")
         cpf = input("CPF: ")
         Email= input("Email: ")
-
-        # data atual para coluna data
+        
         data_cadastro = datetime.datetime.now().strftime("%Y-%m-%d")
 
-        # add dados para Cadastro_temp
         sql = """
         INSERT INTO [Cliente Cadastrados] ( CPF,Nome, Sobrenome, Idade, Email, [Data de Cadastro] )
         VALUES (?, ?, ?, ?, ?, ?)
@@ -54,8 +51,7 @@ def inserir_MaquinasCadastradas():
     try:
         conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
-
-        # input pelo terminal
+        
         nomedamaquina = input("\nNome da maquina: ")
         preço = input("preço: ")
     
@@ -75,7 +71,72 @@ def inserir_MaquinasCadastradas():
         conn.close()
         
 
-# Menu principal-------------------------------------------------------------------------------------------------------
+def cadastrar_login():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        email = input("Digite seu email para cadastro: ")
+        senha = input("Digite sua senha para cadastro: ")
+    
+        #Checa se o e-mail já foi cadastrado
+        cursor.execute("SELECT * FROM [Tabela Login] WHERE email = ?", email)
+        if cursor.fetchone() is not None:
+            print("Email já registrado!")
+            return
+        cursor.execute("INSERT INTO [Tabela Login] (Email, Senha) VALUES (?, ?)", email, senha)
+        conn.commit()
+        print("Cadastro realizado com sucesso!")
+    except pyodbc.Error as e:
+        print("Erro no cadastro:", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+def realizar_login():
+    while True:
+        try:
+            conn = pyodbc.connect(connection_string)
+            cursor = conn.cursor()
+            email = input("Digite seu email: ")
+            senha = input("Digite sua senha: ")
+            cursor.execute("SELECT senha FROM [Tabela Login] WHERE Email = ?", email)
+            row = cursor.fetchone()
+            if row is not None:
+                stored_senha = row[0]
+                if senha == stored_senha:
+                    print("Login realizado com sucesso!")
+                    return True
+                else:
+                    print("Senha incorreta, tente novamente.")
+            else:
+                print("Email não encontrado, tente novamente.")
+        except pyodbc.Error as e:
+            print("Erro no login:", e)
+        finally:
+            cursor.close()
+            conn.close()
+
+def login_menu():
+    while True:
+        print("\n" + "=" * 50)
+        print("LOGIN".center(50))
+        print("=" * 50)
+        opcao_login = input("\n[1] Cadastrar Login\n[2] Fazer Login\n[3] Sair\n\nEscolha uma opção: ")
+        if opcao_login == "1":
+            cadastrar_login()
+        elif opcao_login == "2":
+            if realizar_login():
+                return True
+        elif opcao_login == "3":
+            return False
+        else:
+            print("Opção inválida, tente novamente.")
+
+if not login_menu():
+    print("Encerrando programa...")
+    exit()
+
+# Menu principal ----------------------------------------------------------------------------------
 while True:
     print("\n" + "=" * 50)
     print("MENU PRINCIPAL".center(50))
@@ -87,7 +148,6 @@ while True:
         inserir_dados()
     elif opcao == "M":
         inserir_MaquinasCadastradas()
-  
     elif opcao == "S":
         print("\nSaindo do sistema...")
         break
